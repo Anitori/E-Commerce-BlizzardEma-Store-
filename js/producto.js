bba4cfd11518e1b344bfbbe64eef162d6c9113c6
Emanuel Rodriguez Anitori <88413740+Anitori@users.noreply.github.com>
2021-11-21 21:21:16 -0300

    First Commit

---
+++ js/producto.js
@@ -0,0 +1,11 @@
+let productos = [
+    {stock: 0, id: 1, nombre: "Diablo II", tipo: "Diablo", marca: "lenovo", desc: "Diablo II Resurrected", precio: 1500, img: 'img/diabloII.jpeg'},
+    {stock: 8, id: 2, nombre: "Diablo III", tipo: "Diablo", marca: "lenovo", desc: "Diablo III", precio: 1800, img: 'img/diabloIII.jpeg'},
+    {stock: 3, id: 3, nombre: "Diablo IV", tipo: "Diablo", marca: "lenovo", desc: "Diablo IV", precio: 2200, img: 'img/diabloIV.jpeg'},
+    {stock: 10, id: 4, nombre: "Heroes of the Storm", tipo: "Heroes of the Storm", marca: "cx", desc: "Heroes of the Storm", precio: 1000, img: 'img/hots.jpeg'},
+    {stock: 10, id: 5, nombre: "Hearthstone", tipo: "Hearthstone", marca: "exo", desc: "Hearthstone", precio: 1000, img: 'img/hs.jpeg'},
+    {stock: 1, id: 6, nombre: "Overwatch", tipo: "Overwatch", marca: "hp", desc: "Overwatch", precio: 1000, img: 'img/overwatch.jpeg'},
+    {stock: 10, id: 7, nombre: "Overwatch 2", tipo: "Overwatch", marca: "dev", desc: "Overwatch 2", precio: 1000, img: 'img/overwatch2.jpeg'},
+    {stock: 6, id: 8, nombre: "Starcraft II", tipo: "Starcraft", marca: "dev", desc: "Starcraft II", precio: 1000, img: 'img/starcraftII.jpeg'},
+    {stock: 10, id: 9, nombre: "World of Warcraft", tipo: "wow", marca: "dev", desc: "World of Warcraft", precio: 1000, img: 'img/wow.jpeg'},
+]
